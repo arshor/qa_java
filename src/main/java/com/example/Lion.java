7cf9395cb3b945e07f14c5658c7d1e00d8dc5387
Andrey Shorokhov <arshor@rambler.ru>
2023-04-05 20:39:37 +0500

    Added Tests for Lion class

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,16 +5,17 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
-    private Feline feline;
+    Feline feline;
 
     public Lion(String sex, Feline feline) throws Exception {
         this.feline = feline;
+
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
     }
 
---
+++ src/test/java/com/example/LionTest.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+    List<String> food = List.of("Животные", "Птицы", "Рыба");
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getKittensTest() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        assertEquals(1, lion.getKittens());
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(food);
+        assertEquals(food, lion.getFood());
+    }
+
+    @Test
+    public void lionExceptionTest() {
+        try {
+            Lion lion = new Lion("нечто", feline);
+        } catch (Exception exception) {
+            assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
+        }
+    }
+
+}
